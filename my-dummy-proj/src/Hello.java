b36b224daa9a8fb5028802b4179df7507ab17270
Pranali Taware <pranalitaware@gmail.com>
2021-03-23 19:16:57 +0530

    adding 2nd newline

--- my-dummy-proj/src/Hello.java
+++ my-dummy-proj/src/Hello.java
@@ -2,15 +2,11 @@
 public class Hello {
  public void method1() {
 	 System.out.println("hi");
-
 	 System.out.println("javavava");
-
 	 System.out.println("hello");
 	 System.out.println("hiiii");
-
-
 	 System.out.println("dddddd");
-
+	 System.out.println("adding 2nd newline");
 
  }
 }
